5d564bfcfa770ffd97c23e055394330faef6fbe6
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-03 01:21:00 -0500

    s

---
+++ cmake-build-debug/CMakeFiles/Parallel_Homework3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parallel_Homework3.dir/main.cpp.o"
+  "Parallel_Homework3"
+  "Parallel_Homework3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parallel_Homework3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
