971bcddcf2cf6fbbd3c9fc3a46b77a9ee563b697
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-06 21:40:06 -0800

    Update main.c

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
     ll* in = calloc(blck, sizeof(ll));
     ll* rec = calloc(blck, sizeof(ll));
 
-    const int freq =  512000000;
+    const double freq =  512000000;
 
     //pre-comp value
     ll val = blck * rank;
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
         printf("Value Norm: %lld\n", solution);
         printf("Time P2P: %llu\n", ((double)end_time1 - (double)start_time1)/freq);
         printf("Time NORM: %llu\n", ((double)end_time2 - (double)start_time2));
-        printf("Time calc: %llu\n", ((double)end_time2 - (double)start_time2) / freq);
+        printf("Time calc: %d\n", ((double)end_time2 - (double)start_time2) / freq);
 
     }
 
