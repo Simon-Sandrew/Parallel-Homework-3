1c9daca44454772d9941232aef678e3b50020358
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-06 20:13:55 -0800

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ void MPI_P2P_REDUCE(ll *sendbuf, ll *recvbuf, int count, MPI_Datatype datatype, 
     while(stride < size){
         ll curr = 0;
 
-        if(rank % (2 * stride) > stride){
+        if(rank % (2 * stride) == stride){
             int dest = rank - stride;
             MPI_Isend(recvbuf, 1, MPI_LONG_LONG, dest, 0, comm, &req);
             MPI_Wait(&req, MPI_STATUS_IGNORE);
@@ -41,32 +41,32 @@ void MPI_P2P_REDUCE(ll *sendbuf, ll *recvbuf, int count, MPI_Datatype datatype, 
 }
 
 int main(int argc, char *argv[]) {
-        int rank;
-        int size;
-        
-        MPI_Init(&argc, &argv);
-        
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-       
+    int rank;
+    int size;
+    
+    MPI_Init(&argc, &argv);
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
 
-        const int BLOCK_SIZE = (1 << 30) / size;
+    const int BLOCK_SIZE = (1 << 30) / size;
 
-        ll* in = calloc(BLOCK_SIZE, sizeof(ll));
-        ll* rec = calloc(BLOCK_SIZE, sizeof(ll));
-        ll val = BLOCK_SIZE * rank;
-        
-        for(int i = 0; i < BLOCK_SIZE; ++i){
-            in[i] = val++;
-        }
+    ll* in = calloc(BLOCK_SIZE, sizeof(ll));
+    ll* rec = calloc(BLOCK_SIZE, sizeof(ll));
+    ll val = BLOCK_SIZE * rank;
+    
+    for(int i = 0; i < BLOCK_SIZE; ++i){
+        in[i] = val++;
+    }
 
-        MPI_P2P_REDUCE(in, rec, BLOCK_SIZE, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
-       
-        if(rank == 0){
-            printf("%lld", rec[0]);
-        }
-        MPI_Finalize();
-        return 0;
+    MPI_P2P_REDUCE(in, rec, BLOCK_SIZE, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    if(rank == 0){
+        printf("%lld", rec[0]);
+    }
+    MPI_Finalize();
+    return 0;
 
 }
