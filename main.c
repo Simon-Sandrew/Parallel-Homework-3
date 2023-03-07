578bfaddaed5f6aaef38316b2a1d98d7eefbc04e
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-06 21:42:16 -0800

    Update main.c

--- main.c
+++ main.c
@@ -87,9 +87,9 @@ int main(int argc, char *argv[]) {
     if(rank == 0){
         printf("Value P2P: %lld\n", rec[0]);
         printf("Value Norm: %lld\n", solution);
-        printf("Time P2P: %llu\n", ((double)end_time1 - (double)start_time1)/freq);
-        printf("Time NORM: %llu\n", ((double)end_time2 - (double)start_time2));
-        printf("Time calc: %d\n", ((double)end_time2 - (double)start_time2) / freq);
+        printf("Time P2P: %llu\n", (double)(end_time1 - start_time1));
+        printf("Time NORM: %llu\n", (double)(end_time2 - start_time2));
+        printf("Time calc: %llu\n", (double)(end_time2 - start_time2) / (double)freq);
 
     }
 
