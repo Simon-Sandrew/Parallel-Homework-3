1c1830a593a85f7428681f5832da35ae41443b08
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-06 18:50:21 -0800

    Update main.c

--- main.c
+++ main.c
@@ -3,41 +3,48 @@
 #include <stdio.h>
 #define ll long long
 
-void MPI_P2P_REDUCE(ll *sendbuf, ll *recvbuf, int count, int root, int argc, char* argv[]){
+void MPI_P2P_REDUCE(ll *sendbuf, ll *recvbuf, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
+    int rank;
+    int size;
 
-  
-    MPI_Init(NULL, NULL);
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    printf("%d", world_size);
-    //stride is the size of the groups. We start with single groups of 1. Then we reduce by increasing the size of the stride.
-    int stride  = 1;
-    int mod = 2 * stride;
-    while(stride != world_size){
-        mod = 2 * stride;
-        int rank;
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        if(rank % stride == 0 && rank % mod == 0){
-            //if rank is divisble by strid AND MOD then it is a reciever, meaning evaluation will be done. 
-            //not sure what address we recieve from but it exists
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
 
-
-
-            ll* buff;
-            //MPI_Irecv(buff, 1, MPI_LONG_LONG, sendbuf + stride, 0, MPI_COMM_WORLD, MPI_REQUEST_NULL);
-            //do nothing
-        }
-        else if(rank % mod == 0){
-            //needs to send to the nearest the address of current rank - stride, so for pair (0, 4), (8, 12)... 4 sends to 0, 12 sends to 8.  
-        }
-        stride *= 2; 
-        //before continuing we need to wait for all ranks to finish this iteration of the loop
-        MPI_Finalize();
+    MPI_Request req;
+    recvbuf[0] = 0;
+    for(int i = 0; i < count; ++i){
+        recvbuf[0] += sendbuf[i];
     }
+
+    int stride = 1;
+    while(stride < size){
+        ll curr = 0;
+        if(rank % (2 * stride) == 0){
+            MPI_Irecv(&curr, 1, MPI_LONG_LONG, rank + stride, 0, comm, &req);
+            MPI_Wait(&req, MPI_STATUS_IGNORE);
+            recvbuf[0] += curr;
+        }
+        
+        if(rank % (2 * stride) == stride){
+            int dest = rank - stride;
+            MPI_Isend(recvbuf, 1, MPI_LONG_LONG, dest, 0, comm, &req);
+            MPI_Wait(&req, MPI_STATUS_IGNORE);
+        }
+
+
+        stride *= 2;
+    }
+
 }
 
 int main(int argc, char *argv[]) {
+        MPI_Init(&argc, &argv);
         MPI_P2P_REDUCE(NULL, NULL, 0, 0, argc, argv);
+        MPI_Wait(NULL, NULL);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        if(rank == 0){
+            printf();
+        }
         return 0;
 
 }
