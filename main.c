38300c0508ebfde5b4ef865e494d367aa17a0356
Simon-Sandrew <sandrewsimon@gmail.com>
2023-03-06 22:06:05 -0800

    Revert "Update main.c"
    This reverts commit 19e18ba771068c7474b84e9bdcab15c8bec3e28c.

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
         printf("Value Norm: %lld\n", solution);
         printf("Time P2P: %llu\n", ((double)end_time1 - (double)start_time1)/freq);
         printf("Time NORM: %llu\n", ((double)end_time2 - (double)start_time2));
-        printf("Time calc: %llu\n", ((double)end_time2 - (double)start_time2) / freq);
+        printf("Time calc: %llu\n", ((double)end_time2 - (double)start_time2));
 
     }
 
